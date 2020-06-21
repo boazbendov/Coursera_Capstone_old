c5c2a7502f491c5cdf5005419c3c9e6e63ac53c5
boazbendov <66850351+boazbendov@users.noreply.github.com>
2020-06-22 01:18:22 +0300

    Add files via upload

---
+++ The Battle of Neighborhoods.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project "The Battle of Neighborhoods"
+
+# In[3]:
+
+
+conda install pandas
+
+
+# In[4]:
+
+
+conda install numpy
+
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
